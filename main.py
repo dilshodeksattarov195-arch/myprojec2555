a92b37281bcfb76aaca2ef85c7825d83d4140624
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 12:23:15 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cartPalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cartPalculateWorker()
+    print("Worker engine initialized.")
